b28f9a6ab69c2b2c2e360f203c1202db38138a91
Yeming on main Computer <492851378@qq.com>
2018-02-06 00:02:13 +0800

    improvement

--- data_getter.py
+++ data_getter.py
@@ -9,14 +9,18 @@ def read_contents_from_readhub():
     res = requests.get('https://readhub.me/')
     soup = bs4.BeautifulSoup(res.text)
     items = soup.select('div[class="topicItem___3YVLI"]')
-    contents = {each_item.select('h2 span')[0].text: each_item.select('div[class="summary___1i4y3"] div')[0].text
+    contents = {each_item.select('h2 span')[0].text: each_item.select('div[class="summary___1i4y3"] div')[0].text[0:80]
+                + '...\n' + each_item.select('a')[0].get('href')
                 for each_item in items}
+    # contents = {each_item.select('h2 span')[0].text: each_item.select('a')[0].get('href')
+    #             for each_item in items}
 
     return contents
 
-#
-# cts = read_contents_from_readhub()
-#
-# for k, v in cts.items():
-#     print("%s:\n%s" % (k, v))
-#     print('----------------------------------------')
+
+if __name__ == '__main__':
+    cts = read_contents_from_readhub()
+
+    for k, v in cts.items():
+        print("%s:\n%s" % (k, v))
+        print('----------------------------------------')
--- robot.py
+++ robot.py
@@ -5,8 +5,15 @@ contents = data_getter.read_contents_from_readhub()
 str_cts = []
 for k, v in contents.items():
     str_cts.append('【%s】\n%s\n' % (k, v))
-show_cts = '------------------\n'.join(str_cts)
+show_cts = '------------------------\n'.join(str_cts)
 
 # bot = wxpy.Bot()
+# me = bot.self
+# me.send(show_cts)
+# gs = bot.groups()
+# for k, v in enumerate(gs):
+#     print('%d、%s' % (k, v))
+# g = gs[int(input())]
+# g.send(show_cts)
 
 print(show_cts)
