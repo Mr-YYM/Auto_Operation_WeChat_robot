c5bf56ab71a9822e3c69b5ee8ca837da43f70c6c
Yeming on main Computer <492851378@qq.com>
2018-02-09 21:39:19 +0800

    Basically completed

--- robot.py
+++ robot.py
@@ -32,7 +32,9 @@ def send_contents(sending_cts, member):
 
 if __name__ == '__main__':
     bot = wxpy.Bot(cache_path=True, console_qr=True)
-    me = bot.self
+    gs = bot.groups()
+    me = gs[3]
+    print(me.name)
     times = 0
     while 1:
         times += 1
