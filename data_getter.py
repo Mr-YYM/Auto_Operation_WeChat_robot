423d14c65951f9ed8a229bf963168d6d6e5e4c9e
Yeming on main Computer <492851378@qq.com>
2018-03-07 20:02:51 +0800

    increment

--- data_getter.py
+++ data_getter.py
@@ -10,21 +10,21 @@ warnings.filterwarnings('ignore')
 def read_contents_from_readhub():
     try:
         res = requests.get('https://readhub.me/')
+        soup = bs4.BeautifulSoup(res.text)
+        items = soup.select('div[class="topicItem___3YVLI"]')
+        contents = {
+            each_item.select('h2 span')[0].text:
+                {
+                     'date_time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),
+                     'content': each_item.select('div[class="summary___1i4y3"] div')[0].text,
+                     'link': each_item.select('a')[0].get('href')
+                }
+            for each_item in items
+        }
+
+        return contents
     except Exception as exp:
         logging.error("发生了一个错误：%s" % exp)
-    soup = bs4.BeautifulSoup(res.text)
-    items = soup.select('div[class="topicItem___3YVLI"]')
-    contents = {
-        each_item.select('h2 span')[0].text:
-            {
-                 'date_time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),
-                 'content': each_item.select('div[class="summary___1i4y3"] div')[0].text,
-                 'link': each_item.select('a')[0].get('href')
-            }
-        for each_item in items
-    }
-
-    return contents
 
 
 if __name__ == '__main__':
