3521e009e691cf465da1768d58cc3d9ee93c8b9d
Yeming on main Computer <492851378@qq.com>
2018-03-13 20:43:44 +0800

    improvement

--- robot.py
+++ robot.py
@@ -2,6 +2,7 @@ import wxpy
 import data_getter
 import time
 import logging
+from threading import Thread
 
 
 def send_contents(contents, member):
@@ -20,33 +21,28 @@ def send_contents(contents, member):
         print('nothing')
 
 
-def send_news_to_groups(a_bot):
-    me = a_bot.search('【资讯】广海互联网社群')[0]
-    # me = bot.self
-    print(me.name)
-
+def send_news_to_groups(a_group, interval):
     times = 0
     while 1:
         times += 1
-        print("☆☆群【%s】开始进行第%d轮action☆☆\n%s" % (me.name, times, '-' * 60))
+        print("☆☆群【%s】开始进行第%d轮action☆☆\n%s" % (a_group.name, times, '-' * 60))
 
         to_send_cts = data_getter.get_send_cts()
+        print("☆☆刚刚获取了一些信息☆☆\n%s" % ('-' * 60))
 
         try:
-            send_contents(to_send_cts, me)
-            print("☆☆刚刚获取了一些信息☆☆\n%s" % ('-' * 60))
+            send_contents(to_send_cts, a_group)
         except wxpy.ResponseError as exp:
             if exp.err_code == 1100 or 1101 or 1102:
                 print('☆☆账号异常退出，请重新登录☆☆')
-                a_bot = wxpy.Bot(console_qr=1)
             else:
                 print('发生了一些错误：', exp)
-                break
-        time.sleep(900)
+
+        time.sleep(interval*60)
 
 
 if __name__ == '__main__':
-    bot = wxpy.Bot()
+    bot = wxpy.Bot(cache_path=True)
     s_keys = {'产品': '【产品】广海互联网细分群',
               '设计': '【设计】广海互联网细分群',
               '运营': '【运营】广海互联网细分群',
@@ -75,4 +71,14 @@ if __name__ == '__main__':
             else:
                 return '你已经在群里边了吧'
 
-    send_news_to_groups(bot)
+
+    g_info = bot.search('【资讯】广海互联网社群')[0]
+    g_test = bot.search('机器人测试')[0]
+    # me = bot.self
+    print(g_info.name)
+
+    t = Thread(target=send_news_to_groups, args=(g_info, 15,))
+    t.start()
+
+    t2 = Thread(target=send_news_to_groups, args=(g_test, 1,))
+    t2.start()
